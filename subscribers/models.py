f0b43aa8dec9da485652f98113a4ec8f9bf343a0
Kalyan Kumar <kumarkalyan.1998@gmail.com>
2018-01-29 23:42:34 +0530

    First Commit

---
+++ subscribers/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Subscriber(models.Model):
+    user_rec = models.ForeignKey(User)
+    address_one = models.CharField(max_length=100)
+    address_two = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=20)
+    stripe_id = models.CharField(max_length=30, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'subscribers'
+
+    def __unicode__(self):
+        return u"%s's Subscription Info" % self.user_rec
